a0d358fe83662cf7a5fa605dba9d3dacccbeba42
Gema Martin <geminway@live.com>
2022-05-10 01:16:03 +0200

    Added the netlify routes and fixed the emits that did not need this.

--- src/components/NavbarBase.vue
+++ src/components/NavbarBase.vue
@@ -6,7 +6,7 @@
 <template>
     <div class="navbar-base">
         <div class="site-logo">
-            <button @click="this.$emit('triggerMenu')">
+            <button @click="$emit('triggerMenu')">
                 <span>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path class="bar bar1" d="M20.05 11H3.95C3.42533 11 3 11.4253 3 11.95V12.05C3 12.5747 3.42533 13 3.95 13H20.05C20.5747 13 21 12.5747 21 12.05V11.95C21 11.4253 20.5747 11 20.05 11Z" fill="white"/>
