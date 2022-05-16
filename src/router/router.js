7cf37336e3dd94365f24518f404a6082afa7ad83
Gema Martin <geminway@live.com>
2022-05-16 12:56:12 +0200

    Changed names that were in Spanish

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import About from '../views/About.vue';
-import MenuCarta from '../views/MenuCarta.vue';
-import MenuDia from '../views/MenuDia.vue';
-import CartaBebidas from '../views/CartaBebidas.vue';
+import Main from '../views/Main.vue';
+import MainDay from '../views/MainDay.vue';
+import MainDrinks from '../views/MainDrinks.vue';
 
 const routes = [
     { path: '/', name: 'about', component: About },
-    { path: '/main', name: 'main', component: MenuCarta },
-    { path: '/main-drinks', name: 'main-drinks', component: CartaBebidas },
-    { path: '/main-day', name: 'main-day', component: MenuDia },
+    { path: '/main', name: 'main', component: Main },
+    { path: '/main-drinks', name: 'main-drinks', component: MainDrinks },
+    { path: '/main-day', name: 'main-day', component: MainDay },
 ]
 
 const router = createRouter({
---
+++ src/views/MainDay.vue
@@ -0,0 +1,35 @@
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import BaseMainDishes from "../components/BaseMainDishes.vue";
+
+let menuDay = ref({
+  nameMenu: 'Menú del día',
+  dishes: [
+    { nameDishes: 'Parrillada de verduras con queso de Ronda y virutas de ibérico', 
+      ingredients: 'Berenjena, Calabacín, Tomate, Pimiento rojo y verde, Espárrago verde, Queso Ronda, Nata, Queso untar, Sal, Pimienta blanca'
+    },
+    { nameDishes: 'Ajo blanco con tartar de gambas marinadas en cebollino y menta', 
+      ingredients: 'Ajo, AOVE, Gambas, Cebollino, Menta'
+    },
+    { nameDishes: 'Tronco de bacalao con muselina de pimientos y espaguetis de calabacín', 
+      ingredients: 'Berenjena, Calabacín, Tomate, Pimiento rojo y verde, Espárrago verde, Queso Ronda, Nata, Queso untar, Sal, Pimienta blanca'
+    },
+    
+    
+  ] 
+})
+</script>
+
+<template>
+  <div class="menu-dia">
+    <base-main-dishes :menuObject="menuDay" />
+  </div>
+</template>
+<style>
+.menu-dia{
+  position: relative;
+  top: 4em;
+  height: 100vh;
+}
+</style>
