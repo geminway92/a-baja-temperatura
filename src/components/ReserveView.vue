e0b1df6989178adc0c4b4ce69d41eef554d54c39
Gema Martin <geminway@live.com>
2022-06-01 09:11:58 +0200

    Fixed bug that did not put the current date in the min attribute of input date. This was failing because the day was 01 but it printed 1 and did not detect it as valid format.

--- src/components/ReserveView.vue
+++ src/components/ReserveView.vue
@@ -199,7 +199,7 @@ const inputFormUser = [
 
         const dateActual = () => {
             const date = new Date()
-            const day = date.getDate()
+            const day = date.getDate().toString().padStart(2,0)
             const month = (date.getMonth() + 1).toString().padStart(2,0)
             const year = date.getFullYear().toString().padStart(2,0)
 
